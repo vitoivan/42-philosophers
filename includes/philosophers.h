4933ae44758f020477a4a349278dc625f34fc6b3
Victor Simoes <victor.simoes@brmalls.com.br>
2023-04-04 11:02:57 -0300

    putting everything in subject norms

--- includes/philosophers.h
+++
@@ -1,9 +0,0 @@
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
-
-# include "./utils.h"
-# include <pthread.h>
-# include <stdio.h>
-# include <stdlib.h>
-
-#endif
--- includes/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-# define UTILS_H
-
-void	print_usage_message(void);
-
-#endif
